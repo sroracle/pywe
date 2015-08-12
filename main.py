2d718345069bbbab88d9fa8af4c18bea63459269
sroracle <maxcrees@me.com>
2015-08-12 12:49:58 -0500

    Move to Python 3; PmConfig.parsepage() is now PmPage.__init__()
    * Change of style for one-line block statements
    * PmConfig.parsepage() moves to PmPage.__init__()
    * Change of nomenclature for PmPage properties in re. the page's name
      * PmPage.name (formerly PmConfig.page) is the fully qualified page name
      * PmPage.shortname (formerly anonymous page_name) is the page name's stub
      * PmPage.group (formerly anonymous page_group) is the page's group
      * PmPage.url (formerly PmConfig.url) is the formatted pretty URL for the page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 # vim: sw=4:ts=4:sts=4
 # Python-based PmWiki Editor (pywe)
 # =================================
@@ -10,17 +10,17 @@
 # Software version: v1.3 - Public Release February 7, 2007
 # Software version: v1.3.1 - Public Release February 9, 2007
 __version__ = 'v1.3.1 - February 9, 2007'
-from   ConfigParser import ConfigParser, DEFAULTSECT
-from   getpass      import getpass
-import logging      #      basicConfig, error, info
-from   argparse     import ArgumentParser
-import os           #      path, system
-import re           #      compile, sub
-import sys          #      argv, exit, path, stderr
-from   tempfile     import NamedTemporaryFile
-from   time         import strftime
-from   urllib       import urlencode, urlopen
-from   urlparse     import urljoin, urlsplit
+from   configparser   import ConfigParser, DEFAULTSECT
+from   getpass        import getpass
+import logging        #      basicConfig, error, info
+from   argparse       import ArgumentParser
+import os             #      path, system
+import re             #      compile, sub
+import sys            #      argv, exit, path, stderr
+from   tempfile       import NamedTemporaryFile
+from   time           import strftime
+from   urllib.parse   import urlencode, urljoin, urlsplit
+from   urllib.request import urlopen
 
 #===================================
 # Various Custom Exception Classes.
@@ -34,7 +34,7 @@ class NoEditor(Exception):
 
 class NoSource(Exception):
     def __init__(self):
-        fatal('No local source file to read from.')
+        fatal('No local source file to read from')
 
 class TempWriteError(Exception):
     def __init__(self):
@@ -42,22 +42,21 @@ class TempWriteError(Exception):
 
 class MultipleServers(Exception):
     def __init__(self):
-        fatal('No server specified and multiple servers are configured, not guessing.')
+        fatal('No server specified and multiple servers are configured, not guessing')
 
 class NoServers(Exception):
     def __init__(self):
-        fatal("You don't have any servers configured.")
+        fatal("You don't have any servers configured")
 
 class RequiredSetting(Exception):
     def __init__(self, setting):
-        fatal('The setting "' + setting + '" is required.')
+        fatal('The setting "' + setting + '" is required')
 
 #===================================
 # PmWiki Configuration Class
 #-----------------------------------
 class PmConfig:
-    def __init__(self, server, api):
-        self.api = api
+    def __init__(self, server):
         c = self._config = ConfigParser()
         self.file = os.path.expanduser('~/.config/pywe.ini')
 
@@ -79,15 +78,19 @@ class PmConfig:
             if len(self.servers) == 1:
                 self.server = server = self.servers[0]
                 log('No server specified, using "'+self.server+'"')
-            elif len(self.servers) > 1: raise MultipleServers
-            else: raise NoServers
+            elif len(self.servers) > 1:
+                raise MultipleServers
+            else:
+                raise NoServers
 
         for setting in layout.keys():
             if c.has_section(server) and c.has_option(server, setting):
-                if type(layout[setting]) is str: val = c.get(server, setting)
-                elif type(layout[setting]) is bool: val = c.getboolean(server, setting)
-                elif layout[setting] is RequiredSetting: val = c.get(server, setting)
-
+                if type(layout[setting]) is str:
+                    val = c.get(server, setting)
+                elif type(layout[setting]) is bool:
+                    val = c.getboolean(server, setting)
+                elif layout[setting] is RequiredSetting:
+                    val = c.get(server, setting)
             else:
                 if layout[setting] is RequiredSetting: raise RequiredSetting(setting)
                 else: val = layout[setting]
@@ -96,53 +99,62 @@ class PmConfig:
 
         if not self.url: self.url = self.api
 
-    def parsepage(self, page):
-        if not page: page = self.page
-        page = page.replace('.pmwiki', '')
-        page_group = page.split('.')[0]
-        try: page_name = page.split('.')[1]
-        except IndexError:
-            log('No group specified, using "Main"')
-            page_name = page_group
-            page_group = self.defaultgroup
-            self.page = page_group + '.' + page_name
-        self.url = self.url.replace('$Group', page_group).replace('$Name', page_name)
-        self.page = page
-
 #===================================
 # PmWiki Page Class
 #-----------------------------------
 class PmPage:
-    def __init__(self, config):
+    def __init__(self, config, name):
         self.c = config
-
-    def api(self, action=''):
-        if not action: return self.c.api
+        if name:
+            self.name = name
         else:
-            page = self.c.page.replace('/', '.')
+            self.name = self.c.page
+
+        self.file = self.name
+        if self.name.rfind('.pmwiki') != -1:
+            self.name = self.name.replace('.pmwiki', '')
+
+        self.group = self.name.split('.')[0]
+        try:
+            self.shortname = self.name.split('.')[1]
+        except IndexError:
+            log('No group specified, using "Main"')
+            self.shortname = self.group
+            self.group = self.c.defaultgroup
+            self.name = self.group + '.' + self.shortname
+
+        self.url = self.c.url.replace('$Group', self.group).replace('$Name', self.shortname)
+
+    def api(self, action = ''):
+        if not action:
+            return self.c.api
+        else:
+            page = self.name.replace('/', '.')
             api = self.c.api.rstrip('/')
             return '%s?n=%s&action=%s' % (api, page, action)
 
-    def pull(self, author='', passwd=None):
+    def pull(self, author = '', passwd = None):
         '''Retrieves the PmWiki source from the website'''
         url = self.api('source')
-        payload = urlencode({'authid' : author, 'authpw' : passwd})
+        payload = urlencode({'authid' : author, 'authpw' : passwd}).encode()
         try:
             fh  = urlopen(url, payload)
-            content = fh.read()
+            content = fh.read().decode()
             if content.startswith('<!DOCTYPE'): raise Unauthorized(url)
             else: return content
-        except IOError: fatal('Could not access', url)
+        except IOError:
+            fatal('Could not access', url)
 
     def push(self, old, new, author = '', passwd = None):
         '''Writes the page back to the website'''
-        if old == new: log("Aborting write: you didn't make any changes!")
+        if new == old:
+            log("Aborting write: you didn't make any changes!")
         else:
             url = self.api('edit')
             payload = {
                 'action': 'edit',
                 'author': author,
-                'n':      self.c.page,
+                'n':      self.name,
                 'post':   1,
                 'text':   new
             }
@@ -150,15 +162,16 @@ class PmPage:
                 payload['authid'] = author
                 payload['authpw'] = passwd
 
-            payload = urlencode(payload)
-            try: post  = urlopen(url, payload)
-            except IOError, e:
+            payload = urlencode(payload).encode()
+            try:
+                post  = urlopen(url, payload)
+            except IOError as e:
                 fn = self.cache(new)
                 msg = 'Failed to write to website: %s\nChanges saved to "%s"' % (e, fn)
                 fatal(msg)
 
     def cache(self, text):
-        name = '%s.pmwiki' % self.c.page.replace('/', '.')
+        name = '%s.pmwiki' % self.name.replace('/', '.')
         f = open(name, 'w')
         f.write(text)
         f.close()
@@ -167,16 +180,17 @@ class PmPage:
     def open(self, editor, text):
         '''Open the page in your favorite editor'''
         if len(text) == 0:
-            log('New page:', self.c.page)
-            text = '(:comment %s is a new page, save as empty to abort:)' % self.c.page
+            log('New page:', self.name)
+            text = '(:comment %s is a new page, save as empty to abort:)' % self.name
 
-        f = NamedTemporaryFile('r+w', -1, '.pmwiki', self.c.page + '-')
+        f = NamedTemporaryFile('w+', suffix = '.pmwiki', prefix = self.name + '-')
         log('Using tempfile', f.name)
         try:
             f.write(text)
             f.flush()
             f.seek(0)
-        except IOError: raise TempWriteError
+        except IOError:
+            raise TempWriteError
 
         cmd = editor + ' ' + f.name
         os.system(cmd)
@@ -200,15 +214,16 @@ def fatal(*args):
 
 def checksetting(command):
     path = command.split(' ',1)[0]
-    if not os.path.isfile(path): fatal("Couldn't find", path)
+    if not os.path.isfile(path):
+        if path.find('/') == -1:
+            fatal('The program "' + path + '" must be specified as an absolute path')
+        else:
+            fatal("Couldn't find", path)
 
 #===================================
 # Main:
 #-----------------------------------
 def main(argv=[]):
-    page = None
-    api = None
-
     p = ArgumentParser()
     p.add_argument(
         '-a', '--author',
@@ -240,60 +255,66 @@ def main(argv=[]):
         'page', metavar='<page>',
         help = 'The page with which to work. This is not required for the "list" command.')
     option = p.parse_args()
-    c = PmConfig(option.server, api)
+    c = PmConfig(option.server)
 
     if option.command == 'list':
-        print 'Configured servers:'
-        print
-        for server in c.servers: print server
+        print('Configured servers:')
+        print()
+        for server in c.servers:
+            print(server)
         sys.exit(0)
 
-    c.parsepage(option.page)
-    wiki = PmPage(c)
+    page = PmPage(c, option.page)
 
     #-----------------------------------
     # Password management
-    if option.nopass: password = None
-    elif c.password and not option.nopass: password = c.password
-    else: password = getpass()
+    if option.nopass:
+        password = None
+    elif c.password and not option.nopass:
+        password = c.password
+    else:
+        password = getpass()
 
     #-----------------------------------
     # Command management
     if option.command == 'push':
         if os.path.isfile(option.page):
-            log('Pushing', c.page, '('+c.url+')')
-            f = open(option.page, 'r')
+            log('Pushing', page.name, '('+page.url+')')
+            f = open(page.file, 'r')
             new = f.read()
             f.close()
             old = ''
-        else: raise NoSource
+        else:
+            raise NoSource
 
     elif option.command == 'delete':
-        delete = raw_input('Deleting: ' + c.page + '. Are you sure? (Type delete)\n')
+        delete = input('Deleting: ' + page.name + '. Are you sure? (Type delete)\n')
         if delete == 'delete':
             old = ''
             new = c.deleteword
         else:
-            print 'Deletion aborted.'
+            print('Deletion aborted.')
             sys.exit(0)
 
     else:
-        old = wiki.pull(c.author, password)
+        old = page.pull(c.author, password)
         if option.command == 'pull':
-            log('Pulling', c.page, '('+c.url+')')
-            log('Saved to', wiki.cache(old))
+            log('Pulling', page.name, '('+page.url+')')
+            log('Saved to', page.cache(old))
             sys.exit(0)
         else:
-            log('Editing', c.page, '('+c.url+')')
+            log('Editing', page.name, '('+page.url+')')
             checksetting(c.editor)
-            new = wiki.open(c.editor, old)
+            new = page.open(c.editor, old)
 
-    if c.keep or option.keep: wiki.cache(new)
-    wiki.push(old, new, c.author, password)
+    if c.keep or option.keep:
+        page.cache(new)
+
+    page.push(old, new, c.author, password)
 
     if option.browse:
         checksetting(c.browser)
-        cmd = '%s %s' % (c.browser, c.url)
+        cmd = '%s %s' % (c.browser, page.url)
         os.system(cmd)
 
 if __name__ == '__main__':
